7857a598349ce99f3d64ae77fc0f166209c82605
Mira Joshi <mirajoshi.1107@gmail.com>
2026-06-08 22:33:34 +0530

    Added a simple for loop with 10 iterations

---
+++ 01_Things_To_Know_In_Any_Language/For_Loops/SimpleForLoop.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    for (int i = 0; i<= 10; i++) {
+        cout << "Hey Meera, this is iteration number  " << i << endl;
+    }
+    return 0;
+}

a5b2a0748dca058deb6fff23b63c25d07c4b00a4
Mira Joshi <mirajoshi.1107@gmail.com>
2026-06-09 14:53:26 +0530

    Added pattern4

---
+++ 01_Things_To_Know_In_Any_Language/Patterns/Pattern4.cpp
@@ -0,0 +1,28 @@
+/*
+1
+2 2
+3 3 3
+4 4 4 4 
+5 5 5 5 5
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern4(int n) {
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int t;
+    cin >> t;
+    for(int i = 0; i < t; i++) {
+        int n;
+        cin >> n;
+        pattern4(n);
+    }
+}
